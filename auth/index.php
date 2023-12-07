7d9a370c0807300390b4d63d7390e12b8771e057
Galym Akishev <galymakishevkz@gmail.com>
2023-12-07 11:23:40 +0600

    Already logged-in user page done

--- auth/index.php
+++ auth/index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 define("NEED_AUTH", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
@@ -7,10 +7,29 @@ if (isset($_REQUEST["backurl"]) && strlen($_REQUEST["backurl"])>0)
 
 $APPLICATION->SetTitle("Авторизация");
 ?>
-<p>Вы зарегистрированы и успешно авторизовались.</p>
- 
-<p>Используйте административную панель в верхней части экрана для быстрого доступа к функциям управления структурой и информационным наполнением сайта. Набор кнопок верхней панели отличается для различных разделов сайта. Так отдельные наборы действий предусмотрены для управления статическим содержимым страниц, динамическими публикациями (новостями, каталогом, фотогалереей) и т.п.</p>
- 
-<p><a href="<?=SITE_DIR?>">Вернуться на главную страницу</a></p>
 
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<div class="container-fluid">
+    <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
+        <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+            <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    <a href="/" class="">
+                        <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+                    </a>
+                </div>
+                <div>
+                    <h3>Вы зарегистрированы и успешно авторизовались</h3>
+                    <p>Используйте административную панель в верхней части экрана для быстрого доступа к функциям управления структурой и информационным наполнением сайта. Набор кнопок верхней панели отличается для различных разделов сайта. Так отдельные наборы действий предусмотрены для управления статическим содержимым страниц, динамическими публикациями (новостями, каталогом, фотогалереей) и т.п.</p>
+                    <p>
+                        Перейти на <a href="/">Главную страницу</a>
+                    </p>
+                    <p>
+                        Перейти на <a href="/personal/">Персональную страницу</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php"); ?>
--- local/templates/main/components/bitrix/system.auth.changepasswd/.default/template.php
+++ local/templates/main/components/bitrix/system.auth.changepasswd/.default/template.php
@@ -1,11 +1,6 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
     die();
-
-if($arResult["PHONE_REGISTRATION"])
-{
-	CJSCore::Init('phone_auth');
-}
 ?>
 
 <div class="container-fluid">
