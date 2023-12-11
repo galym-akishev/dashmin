391c992a49e553592fcfd264f9a17307cef2e604
Galym Akishev <galymakishevkz@gmail.com>
2023-12-11 13:40:47 +0600

    Charts in personal page done

--- local/templates/main/assets/js/main.js
+++ local/templates/main/assets/js/main.js
@@ -58,63 +58,6 @@
         nav : false
     });
 
-
-    // Worldwide Sales Chart
-    var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
-    var myChart1 = new Chart(ctx1, {
-        type: "bar",
-        data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
-                    label: "США",
-                    data: [15, 30, 55, 65, 60, 80, 95],
-                    backgroundColor: "rgba(0, 156, 255, .7)"
-                },
-                {
-                    label: "Великобритания",
-                    data: [8, 35, 40, 60, 70, 55, 75],
-                    backgroundColor: "rgba(0, 156, 255, .5)"
-                },
-                {
-                    label: "Австралия",
-                    data: [12, 25, 45, 55, 65, 70, 60],
-                    backgroundColor: "rgba(0, 156, 255, .3)"
-                }
-            ]
-            },
-        options: {
-            responsive: true
-        }
-    });
-
-
-    // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
-    var myChart2 = new Chart(ctx2, {
-        type: "line",
-        data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
-                    label: "Продажи",
-                    data: [15, 30, 55, 45, 70, 65, 85],
-                    backgroundColor: "rgba(0, 156, 255, .5)",
-                    fill: true
-                },
-                {
-                    label: "Доход",
-                    data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(0, 156, 255, .3)",
-                    fill: true
-                }
-            ]
-            },
-        options: {
-            responsive: true
-        }
-    });
-    
-
-
     // Single Line Chart
     var ctx3 = $("#line-chart").get(0).getContext("2d");
     var myChart3 = new Chart(ctx3, {
@@ -125,7 +68,7 @@
                 label: "Продажи",
                 fill: false,
                 backgroundColor: "rgba(0, 156, 255, .3)",
-                data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
+                data: JSON.parse(localStorage["sales_data"])
             }]
         },
         options: {
@@ -139,9 +82,9 @@
     var myChart4 = new Chart(ctx4, {
         type: "bar",
         data: {
-            labels: ["Италия", "Франция", "Испания", "США", "Аргентина"],
+            labels: JSON.parse(localStorage["revenue_data_countries"]),
             datasets: [{
-                label: "Продажи",
+                label: "Доходы",
                 backgroundColor: [
                     "rgba(0, 156, 255, .7)",
                     "rgba(0, 156, 255, .6)",
@@ -149,7 +92,7 @@
                     "rgba(0, 156, 255, .4)",
                     "rgba(0, 156, 255, .3)"
                 ],
-                data: [55, 49, 44, 24, 15]
+                data: JSON.parse(localStorage["revenue_data_values"])
             }]
         },
         options: {
@@ -163,7 +106,7 @@
     var myChart5 = new Chart(ctx5, {
         type: "pie",
         data: {
-            labels: ["Италия", "Франция", "Испания", "США", "Аргентина"],
+            labels: JSON.parse(localStorage["revenue_data_countries"]),
             datasets: [{
                 backgroundColor: [
                     "rgba(0, 156, 255, .7)",
@@ -172,7 +115,7 @@
                     "rgba(0, 156, 255, .4)",
                     "rgba(0, 156, 255, .3)"
                 ],
-                data: [55, 49, 44, 24, 15]
+                data: JSON.parse(localStorage["revenue_data_values"])
             }]
         },
         options: {
@@ -186,7 +129,7 @@
     var myChart6 = new Chart(ctx6, {
         type: "doughnut",
         data: {
-            labels: ["Италия", "Франция", "Испания", "США", "Аргентина"],
+            labels: JSON.parse(localStorage["revenue_data_countries"]),
             datasets: [{
                 backgroundColor: [
                     "rgba(0, 156, 255, .7)",
@@ -195,7 +138,7 @@
                     "rgba(0, 156, 255, .4)",
                     "rgba(0, 156, 255, .3)"
                 ],
-                data: [55, 49, 44, 24, 15]
+                data: JSON.parse(localStorage["revenue_data_values"])
             }]
         },
         options: {
--- personal/charts/index.php
+++ personal/charts/index.php
@@ -27,9 +27,17 @@ if (!$USER->IsAuthorized()) {
                 while ($row = $propertyValues->Fetch()) { ?>
                     <ul id="sales_hidden_data" style="display: none;">
                         <?php foreach ($row[1] as $value) { ?>
-                        <li id="sales_hidden_data-<?= $value ?>" data-geo="<?= $value ?>"><?= $value ?></li>
+                            <li id="sales_hidden_data-<?= $value ?>" data-value="<?= $value ?>"><?= $value ?></li>
                         <?php } ?>
                     </ul>
+                    <script>
+                        var li_elements = document.getElementById("sales_hidden_data").getElementsByTagName("li");
+                        var sales_data = [];
+                        for (var i = 0, len = li_elements.length; i < len; i++ ) {
+                            sales_data.push(li_elements[i].getAttribute('data-value'));
+                        }
+                        localStorage["sales_data"] = JSON.stringify(sales_data);
+                    </script>
                     <?php } ?>
             <div class="bg-light rounded h-100 p-4">
                 <h6 class="mb-4">Single Line Chart</h6>
@@ -37,21 +45,39 @@ if (!$USER->IsAuthorized()) {
             </div>
         </div>
         <div class="col-sm-12 col-xl-6">
-            <div class="bg-light rounded h-100 p-4">
-                <h6 class="mb-4">Multiple Line Chart</h6>
-                <canvas id="salse-revenue"></canvas>
-            </div>
-        </div>
-        <div class="col-sm-12 col-xl-6">
+            <?php
+            $propertyValues = CIBlockElement::GetPropertyValues(2, array('ACTIVE' => 'Y'), true);
+            while ($row = $propertyValues->Fetch()) { ?>
+                <ul id="revenue_hidden_data_values" style="display: none;">
+                    <?php foreach ($row[2] as $value) { ?>
+                        <li data-value="<?= $value ?>"><?= $value ?></li>
+                    <?php } ?>
+                </ul>
+                <ul id="revenue_hidden_data_countries" style="display: none;">
+                    <?php foreach ($row[3] as $value) { ?>
+                        <li data-value="<?= $value ?>"><?= $value ?></li>
+                    <?php } ?>
+                </ul>
+                <script>
+                    var li_elements = document.getElementById("revenue_hidden_data_values").getElementsByTagName("li");
+                    var revenue_data_values = [];
+                    for (var i = 0, len = li_elements.length; i < len; i++ ) {
+                        revenue_data_values.push(li_elements[i].getAttribute('data-value'));
+                    }
+                    localStorage["revenue_data_values"] = JSON.stringify(revenue_data_values);
+                </script>
+                <script>
+                    var li_elements = document.getElementById("revenue_hidden_data_countries").getElementsByTagName("li");
+                    var revenue_data_countries = [];
+                    for (var i = 0, len = li_elements.length; i < len; i++ ) {
+                        revenue_data_countries.push(li_elements[i].getAttribute('data-value'));
+                    }
+                    localStorage["revenue_data_countries"] = JSON.stringify(revenue_data_countries);
+                </script>
+            <?php } ?>
             <div class="bg-light rounded h-100 p-4">
                 <h6 class="mb-4">Single Bar Chart</h6>
                 <canvas id="bar-chart"></canvas>
-            </div>
-        </div>
-        <div class="col-sm-12 col-xl-6">
-            <div class="bg-light rounded h-100 p-4">
-                <h6 class="mb-4">Multiple Bar Chart</h6>
-                <canvas id="worldwide-sales"></canvas>
             </div>
         </div>
         <div class="col-sm-12 col-xl-6">
